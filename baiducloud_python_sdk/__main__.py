3c0596ac82f1a165f0dd128adab07eb76703cf9d
chongkuitai <chongkuitai@baidu.com>
2026-01-28 17:08:47 +0800

    Init commit

---
+++ baiducloud_python_sdk/__main__.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+################################################################################
+#
+# Copyright (c) 2026 Baidu.com, Inc. All Rights Reserved
+#
+################################################################################
+"""
+本文件允许模块包以python -m baiducloud_python_sdk方式直接执行。
+
+Authors: chongkuitai(chongkuitai@baidu.com)
+Date:    2026/01/28 17:08:47
+"""
+
+import sys
+from baiducloud_python_sdk.cmdline import main
+sys.exit(main())
