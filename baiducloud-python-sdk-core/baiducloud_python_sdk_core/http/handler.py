815ccca8eee39ed21132947e0f20290f11470113
chongkuitai <chongkuitai@baidu.com>
2026-05-11 11:20:39 +0800

    BCE-JC-32853 core
    Change-Id: I9f20a4033ae7a73e9ab09b496eea66f3b7498321

--- baiducloud-python-sdk-core/baiducloud_python_sdk_core/http/handler.py
+++ baiducloud-python-sdk-core/baiducloud_python_sdk_core/http/handler.py
@@ -128,4 +128,5 @@ def parse_error(http_response, response):
         request_id = response.metadata.get('x-bce-request-id')
         bse = BceServerError(http_response.reason, request_id=request_id)
     bse.status_code = http_response.status
-    raise bse
+    if bse is not None:
+        raise bse
